5d701ab8fac4f9bfc44fa3b2c6535292b9d0d64a
Soraia <soraia@Soraias-iMac.local>
2020-02-05 18:17:25 -0500

    pushed up a few tests

--- campaigns/campaignModel.js
+++ campaigns/campaignModel.js
@@ -6,6 +6,7 @@ module.exports = {
     add,
     update,
     remove,
+    listItemizedMD,
     findItemizeMonetaryDonations,
     addItemizedMD,
     updateItemizedMD,
@@ -40,9 +41,14 @@ function add(campaign){
     .insert(campaign)
 };
 
+function listItemizedMD(){
+    return db('itemize_monetary_donations')
+};
+
 function findItemizeMonetaryDonations(campaignId){
     return db('itemize_monetary_donations')
     .join("campaign", "campaign.id", "itemize_monetary_donations.campaignId")
+    .select("itemize_monetary_donations.id","itemize_monetary_donations.campaignId", "itemize_monetary_donations.itemName", "itemize_monetary_donations.amountNeeded", "campaign.title","campaign.fundingGoals", "campaign.deadline")
     .where("campaign.id", campaignId)
 }
 
@@ -50,12 +56,14 @@ function addItemizedMD(campaignId, donations){
     return db('itemize_monetary_donations')
     .join("campaign", "campaign.id", "itemize_monetary_donations.campaignId")
     .where("campaign.id", campaignId)
+    .select("itemize_monetary_donations.id","itemize_monetary_donations.campaignId", "itemize_monetary_donations.itemName", "itemize_monetary_donations.amountNeeded", "campaign.title","campaign.fundingGoals", "campaign.deadline")
     .insert(donations)
 }
 
 function updateItemizedMD(campaignId, changes){
     return db('itemize_monetary_donations')
     .where("itemize_monetary_donations.id", campaignId)
+    .select("itemize_monetary_donations.id","itemize_monetary_donations.campaignId", "itemize_monetary_donations.itemName", "itemize_monetary_donations.amountNeeded", "campaign.title","campaign.fundingGoals", "campaign.deadline")
     .update(changes)
 }
 
